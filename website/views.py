6faaff63f3e536a8b10cb9cb15afe0a9fdeef3af
Andres Gonzalo Garcia <garciaandresgonzalo@gmail.com>
2023-12-21 14:35:43 -0300

    Actualizacion 2: Problema de register solucionado.

--- website/views.py
+++ website/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all() #toma todo lo de la tabla y lo asigna a esta var.
+    
+    
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +21,7 @@ def home(request):
             messages.success(request, 'There was an error while trying to log in, please try again. :/')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records}) 
     
 def logout_user(request):
     logout(request)
@@ -28,13 +32,17 @@ def register_user(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save() #autentica y guarda
+            form.save()
+			# Authenticate and login
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            user = authenticate(authenticate(request, username=username, password=password))
+            user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, "You hace registered succesfully <3")
+            messages.success(request, "You Have Successfully Registered! Welcome!")
             return redirect('home')
-        else:
-            form = SignUpForm()
-            return render(request, 'register.html', {'form': form})
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
+
